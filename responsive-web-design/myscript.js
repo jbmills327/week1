8e38706c6348e58f2ec1a79751bdf3a5c52f9062
Justin Mills <jbmills@gmail.com>
2016-09-21 22:56:21 +0000

    Responsive page progress

---
+++ responsive-web-design/myscript.js
@@ -0,0 +1,16 @@
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "fast", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "fast", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
